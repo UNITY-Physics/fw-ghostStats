84944c630f9e91ba46430fe9adbbc1301cadf0b0
Emil Ljungberg <ljungberg.emil@gmail.com>
2024-06-28 13:30:20 +0200

    updates

---
+++ ghost/apps.py
@@ -0,0 +1,14 @@
+import subprocess as sp
+import shlex
+import os
+import json
+
+"""
+Collection of GHOST Apps to accomplish various tasks
+"""
+
+### Data input / output ###
+
+
+        
+### Warp masks ###
--- ghost/metrics.py
+++ ghost/metrics.py
@@ -1,21 +1,2 @@
 import numpy as np
 
-def calc_psnr(img1, img2, mask):
-    """
-    Calculate the Peak Signal-to-Noise Ratio (PSNR) between two images.
-
-    Parameters:
-    img1 (ndarray or ants image): The first image.
-    img2 (ndarray or ants image): The second image.
-    mask (ndarray or ants image): The mask indicating the region of interest.
-
-    Returns:
-    float: The PSNR value.
-
-    """
-    I1 = img1[mask==1]
-    I2 = img2[mask==1]
-    MSE = np.sum((I1-I2)**2)/len(I1)
-    R = np.mean([max(I1),max(I2)])
-    psnr = 10*np.log10(R**2/MSE)
-    return psnr
